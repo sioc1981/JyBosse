97f53d7bc839e683e40b2c29d73e02484c1b8834
sioc1981 <sioc1981@PORT-SIOC1981>
2019-02-28 20:30:03 +0100

    init Fmessiaen

---
+++ com.google.battle.project/src/main/java/com/google/battle/project/Slide.java
@@ -0,0 +1,16 @@
+package com.google.battle.project;
+
+import java.util.List;
+
+public class Slide {
+	final List<Photo> photos;
+
+	public Slide(List<Photo> photos) {
+		super();
+		this.photos = photos;
+	}
+
+	public List<Photo> getPhotos() {
+		return photos;
+	}
+}
